3a2a8d42f4c027f2c016da698b856ede8341a7d7
Marcos Figueiredo <74501327+marcos1374@users.noreply.github.com>
2024-03-22 14:54:23 -0300

    Update Faturamento x liquidações.sql

--- Faturamento x liquidações.sql
+++ Faturamento x liquidações.sql
@@ -1,82 +1,45 @@
-select
-Clientes.Nome,
-NFCab.ID,
-NFCab.Emissao,
-NFCab.Numero,
-NFCab.VlrNota,
-NFCobr.idNota,
-NFCobr.MovSeq,
-NFCobr.Valor,
-FaturamentoNota.MovSeq,
-FaturamentoNota.Referencia,
-Movimento.Cliente,
-Movimento.BcoCobr,
-Movimento.Estornado,
-Movimento.Complemento,
-Movimento.Valor/1,
-Movimento.DataEntrada,
-Movimento.Data as Vencimento,
-Movimento.Historico,
-Movimento.Origem,
-Movimento.Documento,
-Movimento.ReferenciaFaturamento,
-Movimento.BxConta,
-Movimento.MotivoBaixa,
-Movimento.BcoCobr,
-Movimento.DataBaixa,
-Movimento.BxDesconto/1,
-Movimento.BxMulta/1,
-Movimento.BxJuros/1,
-Movimento.ValorEntrada+Movimento.BxMulta+Movimento.BxJuros/1
-from NFCab
-left join NFCobr on NFCab.ID=NFCobr.idNota
-left join FaturamentoNota on NFCobr.MovSeq=FaturamentoNota.MovSeq
-right join Movimento on NFCobr.MovSeq=Movimento.Sequencia
-left JOIN Clientes ON Movimento.Cliente=Clientes.Codigo
-WHERE (Movimento.Data BETWEEN '2020-01-01 00:00:00' AND '2020-01-31 23:59:59')
-and Movimento.Estornado = "n"
-and Movimento.Historico = "1"
+SELECT
+  Clientes.Nome,
+  Cobranca.ID,
+  Cobranca.Emissao,
+  Cobranca.Numero,
+  Cobranca.Valor,
+  duplicata.idNota,
+  duplicata.MovSeq,
+  duplicata.Valor,
+  FaturamentoNF.MovSeq,
+  FaturamentoNF.Referencia,
+  SQL.Cliente,
+  SQL.BcoCobr,
+  SQL.Estornado,
+  SQL.Complemento,
+  SQL.Valor/1,
+  SQL.DataEntrada,
+  SQL.Data AS Vencimento,
+  SQL.Historico,
+  SQL.Origem,
+  SQL.Documento,
+  SQL.ReferenciaFaturamento,
+  SQL.BxConta,
+  SQL.MotivoBaixa,
+  SQL.BcoCobr,
+  SQL.DataBaixa,
+  SQL.BxDesconto/1,
+  SQL.BxMulta/1,
+  SQL.BxJuros/1,
+  SQL.ValorEntrada+SQL.BxMulta+SQL.BxJuros/1
+FROM Cobranca
+LEFT JOIN NUPLICATA ON Cobranca.ID=NFCobr.idNota
+LEFT JOIN FaturamentoNF ON DUPLICATA.MovSeq=FaturamentoNF.MovSeq
+RIGHT JOIN SQL ON duplicata.MovSeq=SQL.Sequencia
+LEFT JOIN Clientes ON SQL.Cliente=Clientes.Codigo
+WHERE (SQL.Data BETWEEN '2020-01-01 00:00:00' AND '2020-01-31 23:59:59')
+and SQL.Estornado = "n"
+and SQL.Historico = "1"
 and Clientes.Codigo >0
-and Movimento.Origem in("ser","out")
-group by Movimento.Documento
+and SQL.Origem in("ser","out")
+group by SQL.Documento
 UNION ALL
-select
-Clientes.Nome,
-NFCab.ID,
-NFCab.Emissao,
-NFCab.Numero,
-NFCab.VlrNota,
-NFCobr.idNota,
-NFCobr.MovSeq,
-NFCobr.Valor,
-FaturamentoNota.MovSeq,
-FaturamentoNota.Referencia,
-Movimento.Cliente,
-Movimento.BcoCobr,
-Movimento.Estornado,
-Movimento.Complemento,
-Movimento.Valor/1,
-Movimento.DataEntrada,
-Movimento.Data as Vencimento,
-Movimento.Historico,
-Movimento.Documento,
-Movimento.Origem,
-Movimento.ReferenciaFaturamento,
-Movimento.BxConta,
-Movimento.MotivoBaixa,
-Movimento.BcoCobr,
-Movimento.DataBaixa,
-Movimento.BxDesconto/1,
-Movimento.BxMulta/1,
-Movimento.BxJuros/1,
-Movimento.ValorEntrada+Movimento.BxMulta+Movimento.BxJuros/1
-from NFCab
-left join NFCobr on NFCab.ID=NFCobr.idNota
-left join FaturamentoNota on NFCobr.MovSeq=FaturamentoNota.MovSeq
-right join Movimento on NFCobr.MovSeq=Movimento.Sequencia
-left JOIN Clientes ON Movimento.Cliente=Clientes.Codigo
-where (NFCab.Emissao BETWEEN '2020-01-01 00:00:00' AND '2020-01-31 23:59:59')
-and Movimento.Estornado = "n"
-and Movimento.Historico = "1"
-and Clientes.Codigo >0
-group by Movimento.Documento
+SELECT
+  Clientes.Nome,
+  Cobran
